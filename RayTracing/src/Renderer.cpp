a7a0c7223e9c90d3d9e1eadf418d9cdb9c9b9b62
Lukas Kusik <lukas.kusik@gmail.com>
2022-12-10 13:36:09 +0100

    Use #pragma omp parallel for

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -56,7 +56,7 @@ void Renderer::Render(const Scene& scene, const Camera& camera)
 	if (m_FrameIndex == 1)
 		memset(m_AccumulationData, 0, m_FinalImage->GetWidth() * m_FinalImage->GetHeight() * sizeof(glm::vec4));
 
-#define MT 1
+#define MT 0
 #if MT
 	std::for_each(std::execution::par, m_ImageVerticalIter.begin(), m_ImageVerticalIter.end(),
 		[this](uint32_t y)
@@ -76,8 +76,8 @@ void Renderer::Render(const Scene& scene, const Camera& camera)
 		});
 
 #else
-
-	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
+#pragma omp parallel for schedule(dynamic, 1)
+	for (int y = 0; y < m_FinalImage->GetHeight(); y++)
 	{
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
 		{
